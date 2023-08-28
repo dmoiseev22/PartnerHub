8c9d42e631f12037a41e10b2696e8683ac4d1c47
dmoiseev22 <dmitry.a.moiseev@gmail.com>
2023-08-28 15:18:50 +0200

    Update Search.jsx

--- src/components/search/Search.jsx
+++ src/components/search/Search.jsx
@@ -5,14 +5,16 @@ export default function Search({ handleChange, searchInput, handleSubmit }) {
     return (
         <form autocomplete="off" onSubmit={handleSubmit} className="search-by-article">
             <label htmlFor="search"></label>
-            <input 
-                type="text" 
-                placeholder="Search by code"
-                onChange={handleChange}
-                name="search"
-                value={searchInput}
-            />
-            {/* <button type="submit">Search</button> */}
+            <div>
+                <input 
+                    type="text" 
+                    placeholder="Enter product code here"
+                    onChange={handleChange}
+                    name="search"
+                    value={searchInput}
+                />
+                {/* <button type="submit">Search</button> */}
+            </div>
         </form>
     )
 }
