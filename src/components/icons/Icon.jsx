70760d89f84c61650c4988c5a13d4de461460409
dmoiseev22 <dmitry.a.moiseev@gmail.com>
2023-10-10 12:00:54 +0200

    new fixes

--- src/components/icons/Icon.jsx
+++ src/components/icons/Icon.jsx
@@ -1,3 +1,4 @@
+// PRODUCT RELATED ICONS
 import dry from '../../assets/product-icons/iconos_producto_sueltos_Sin agua.svg'
 import al1 from '../../assets/product-icons/iconos_producto_sueltos_AL1.svg'
 import al2 from '../../assets/product-icons/iconos_producto_sueltos_AL2.svg'
@@ -14,12 +15,17 @@ import cut_off from '../../assets/product-icons/iconos_producto_sueltos_Lijadora
 import drilling_machine from '../../assets/product-icons/iconos_producto_sueltos_Máquina brocas.svg'
 import drilling_machine_high_power from '../../assets/product-icons/iconos_producto_sueltos_PIes.svg'
 import table_saw from '../../assets/product-icons/iconos_producto_sueltos_MAquina de mesa.svg'
-import turbo_laser from '../../assets/product-icons/iconos_producto_sueltos_Turbo laser.svg'
+import turbo_laser from '../../assets/product-icons/iconos_producto_sueltos_Turbo Laser.svg'
 import turbo from '../../assets/product-icons/iconos_producto_sueltos_Turbo.svg'
 import broca from '../../assets/product-icons/iconos_producto_sueltos_broca entera 1.svg'
 import wire from '../../assets/product-icons/iconos_producto_sueltos_hilo 1.svg'
 import precast from '../../assets/product-icons/iconos_producto_sueltos_prefabricados 3.svg'
 
+import low_power from '../../assets/product-icons/iconos_producto_sueltos_Baja potencia.svg'
+import medium_power from '../../assets/product-icons/iconos_producto_sueltos_Media potencia.svg'
+import high_power from '../../assets/product-icons/iconos_producto_sueltos_alta potencia.svg'
+
+// HOME PAGE RELATED ICONS
 import language from '../../assets/fa-icons/language.png'
 import diamond from '../../assets/fa-icons/diamond.png'
 import fast from '../../assets/fa-icons/fast.png'
@@ -28,14 +34,6 @@ import scientist from '../../assets/fa-icons/scientist.png'
 import spain from '../../assets/fa-icons/spain.png'
 import team from '../../assets/fa-icons/team.png'
 import iso from '../../assets/fa-icons/iso.png'
-
-
-
-
-import low_power from '../../assets/product-icons/iconos_producto_sueltos_Baja potencia.svg'
-import medium_power from '../../assets/product-icons/iconos_producto_sueltos_Media potencia.svg'
-import high_power from '../../assets/product-icons/iconos_producto_sueltos_alta potencia.svg'
-
 
 
 const icons = {
